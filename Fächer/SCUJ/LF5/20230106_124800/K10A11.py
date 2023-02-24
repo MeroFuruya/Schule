7ff5024a48b522a5aa96aaa37bb2f65f8ae5b7ef
MeroFuruya <luma.rochen@gmail.com>
2023-02-24 09:50:58 +0100

    done stuff

--- Fächer/SCUJ/LF5/20230106_124800/K10A11.py
+++ Fächer/SCUJ/LF5/20230106_124800/K10A11.py
@@ -4,12 +4,14 @@ def all_combinations(n: int) -> None:
         for j in range(i+1, n+1):
             print(f"player {i} vs {j}")
 
+
 all_combinations(3)
+
 
 # one-liner (ok actually two liner *suffering noises*) for the flex hehe
-def all_combinations(n: int) -> None:
+def all_combinations2(n: int) -> None:
     [print(f"player {i} vs {j}") for i in range(1, n+1) for j in range(i+1, n+1)]
 
-all_combinations(3)
 
+all_combinations2(3)
 
